54d55f60058a8c78559fa0dbf273f7875807ee85
Gustavo Pedrosa <luizgfju@hotmail.com>
2025-07-25 23:53:53 -0300

    initial commit

---
+++ src/main/java/PooProjetoJujutsu/Energia.java
@@ -0,0 +1,8 @@
+package PooProjetoJujutsu;
+
+public interface Energia {
+
+    // Aqui em "interface" não implementa!
+    public void energiaAmaldicoada();
+    public void dominio();
+}
---
+++ src/main/java/PooProjetoJujutsu/MegumiFushiguro.java
@@ -0,0 +1,28 @@
+package PooProjetoJujutsu;
+
+public class MegumiFushiguro extends Feiticeiro implements  Energia {
+
+    // Polimorfismo de sobreescrita do construtor com parametros da Classe "Feiticeiro"
+    public MegumiFushiguro(String nome, int forca, int velocidade) {
+        super(nome, forca,velocidade);
+    }
+
+    //  Polimorfismo de sobreescrita do metodo abstrato da classe abstract "Feiticeiro"
+    @Override
+    public void tecnicaAmaldicoada() {
+        System.out.println("");
+    }
+
+    // Polimorfismo de sobreescrita da "interface"
+    @Override
+    public void energiaAmaldicoada() {
+        System.out.println("Invocação das maldições");
+    }
+
+    // Polimorfismo de sobreescrita da "interface"
+    @Override
+    public void dominio() {
+        System.out.println("Energia escura");
+    }
+
+}
