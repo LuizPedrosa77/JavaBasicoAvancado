af776fb9ee8ccc549fe06a9a30d100e20057097d
Gustavo Pedrosa <luizgfju@hotmail.com>
2025-07-27 17:41:42 -0300

    initial commit

---
+++ src/main/java/Poo12LendoArquivos/Main.java
@@ -0,0 +1,22 @@
+package Poo12LendoArquivos;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            // Essa vai informar onde está o arquivo
+            InputStream is = new FileInputStream("AulaArquivosJava.csv");
+
+            // Esse vai preparar para manipulação
+            InputStreamReader isr;
+
+            // Esse vai pegar os dados
+            BufferedReader br;
+
+        } catch (FileNotFoundException e){
+
+        }
+    }
+}
